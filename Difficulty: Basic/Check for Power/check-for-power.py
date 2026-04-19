cd2936b34a765620bd767326ff2cf06a9f215467
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-04-19 21:56:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for Power/check-for-power.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPower(self, x, y):
+        if x == 1:
+            return y == 1
+        
+        while y % x == 0:
+            y //= x
+        
+        return y == 1
