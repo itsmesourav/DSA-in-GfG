6ea14d8cc6bc708a2ab0b81f6730ece7c3d421c6
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-04-20 12:11:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Derangements/count-derangements.py
@@ -0,0 +1,14 @@
+class Solution:
+    def derangeCount(self, n: int) -> int:
+        if n == 1:
+            return 0
+        if n == 2:
+            return 1
+        
+        prev2, prev1 = 0, 1
+        
+        for i in range(3, n + 1):
+            curr = (i - 1) * (prev1 + prev2)
+            prev2, prev1 = prev1, curr
+        
+        return prev1
