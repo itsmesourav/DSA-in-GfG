d2166f9caba8fb57a50deac436d8a7f89c329546
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-09-11 23:10:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Values with Equal Array Remainders/values-with-equal-array-remainders.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sameMod(self, arr):
+        from math import gcd
+
+        g = 0
+
+        for i in range(1, len(arr)):
+            g = gcd(g, abs(arr[i] - arr[0]))
+
+        if g == 0:
+            return -1
+
+        count = 0
+
+        for i in range(1, int(g ** 0.5) + 1):
+            if g % i == 0:
+                count += 1
+
+                if i != g // i:
+                    count += 1
+
+        return count
